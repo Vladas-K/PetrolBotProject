d3055d48ef0c915118f880ef2cb7e40680c57577
Владас Куодис <vlad250577@yandex.ru>
2024-11-14 23:00:03 +0300

    Add new price change

--- petrolbot.py
+++ petrolbot.py
@@ -41,7 +41,8 @@ class PriceBot:
     def check_price_change(self, context):
         """Проверка изменения цены и отправка уведомлений подписчикам"""
         new_price = self.get_price()
-        if new_price is not None and self.current_price is not None and abs(new_price - self.current_price) >= 1:
+        # if new_price is not None and self.current_price is not None and abs(new_price - self.current_price) >= 1:
+        if new_price is not None and self.current_price is not None and abs(new_price - self.current_price) >= 0.1:
             self.current_price = new_price
             for chat_id in self.subscribers:
                 context.bot.send_message(
