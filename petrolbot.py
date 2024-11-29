8185238e212129af4e008bbf2aeaf3e837827fea
Владас Куодис <vlad250577@yandex.ru>
2024-11-29 16:34:21 +0300

    Add first_name, subscribed_date

--- petrolbot.py
+++ petrolbot.py
@@ -17,7 +17,8 @@ secret_token = os.getenv('TOKEN')
 class PriceBot:
     def __init__(self, token):
         self.bot = Bot(token=token)  # Инициализация бота
-        self.application = Application.builder().token(token).build()  # Инициализация Application
+        self.application = Application.builder().token(
+            token).build()  # Инициализация Application
         self.current_price = None  # Текущая цена
         self.init_db()  # Инициализация базы данных
         self.init_handlers()  # Инициализация обработчиков
@@ -28,7 +29,9 @@ class PriceBot:
         self.cursor = self.conn.cursor()
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS subscribers (
-                chat_id INTEGER PRIMARY KEY
+                chat_id INTEGER PRIMARY KEY,
+                first_name TEXT,
+                subscribed_date TEXT
             )
         ''')
         self.conn.commit()
@@ -42,7 +45,8 @@ class PriceBot:
             response.raise_for_status()  # Проверка на ошибки HTTP
             soup = BeautifulSoup(response.text, 'lxml')
             ai_95_block = soup.find('div', class_='fuel-card border-ai92')
-            price = float(ai_95_block.find('span', itemprop='price').text.replace(',', '.'))
+            price = float(ai_95_block.find(
+                'span', itemprop='price').text.replace(',', '.'))
             return price
         except Exception as e:
             logging.error(f"Ошибка при получении цены: {e}")
@@ -77,13 +81,19 @@ class PriceBot:
     async def start(self, update, context):
         """Обработка команды /start, добавление подписчика и отправка приветственного сообщения"""
         chat = update.effective_chat
-        name = update.message.chat.first_name
+        name = update.message.chat.first_name if update.message.chat.first_name else "Unknown"
         chat_id = chat.id
+        subscribed_date = update.message.date.isoformat(
+        ) if update.message.date else "Unknown"
 
-        self.cursor.execute('INSERT OR IGNORE INTO subscribers (chat_id) VALUES (?)', (chat_id,))
+        self.cursor.execute('''
+            INSERT OR IGNORE INTO subscribers (chat_id, first_name, subscribed_date)
+            VALUES (?, ?, ?)
+        ''', (chat_id, name, subscribed_date))
         self.conn.commit()
 
-        button = ReplyKeyboardMarkup([['Узнать актуальную цену']], resize_keyboard=True)
+        button = ReplyKeyboardMarkup(
+            [['Узнать актуальную цену']], resize_keyboard=True)
         await context.bot.send_message(
             chat_id=chat_id,
             text=f'Спасибо, что вы включили меня, {name}!',
@@ -104,12 +114,14 @@ class PriceBot:
         job_queue = self.application.job_queue
 
         # Запуск задания на проверку изменения цены каждые 60 минут
-        job_queue.run_repeating(self.check_price_change, interval=3600, first=0)
+        job_queue.run_repeating(self.check_price_change,
+                                interval=3600, first=0)
 
         # Обработчик команды /start
         self.application.add_handler(CommandHandler('start', self.start))
         # Обработчик текстовых сообщений
-        self.application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_message))
+        self.application.add_handler(MessageHandler(
+            filters.TEXT & ~filters.COMMAND, self.handle_message))
 
     def run(self):
         """Запуск бота"""
